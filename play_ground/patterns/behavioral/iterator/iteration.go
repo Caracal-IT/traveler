e82dd34547aa5f48f05ce4ea7c24cb27d5b83653
ettienemare <ettienemare@gmail.com>
2026-02-09 08:16:15 +0200

    add behavioral iterator pattern example: implement generator-based person names iteration

--- play_ground/patterns/behavioral/iterator/iteration.go
+++ play_ground/patterns/behavioral/iterator/iteration.go
@@ -10,9 +10,24 @@ func (p *Person) Names() []string {
 	return []string{p.FirstName, p.MiddleName, p.LastName}
 }
 
+func (p *Person) NamesGenerator() <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer close(ch)
+		for _, name := range []string{p.FirstName, p.MiddleName, p.LastName} {
+			ch <- name
+		}
+	}()
+	return ch
+}
+
 func main() {
 	p := Person{"Alexander", "Graham", "Bell"}
 	for _, name := range p.Names() {
+		fmt.Println(name)
+	}
+
+	for name := range p.NamesGenerator() {
 		fmt.Println(name)
 	}
 }
